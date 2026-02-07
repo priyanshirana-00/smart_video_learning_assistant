4b7224f6964854f49ecc4265524deff96a4ef9b2
priyanshirana-00 <priyanshirana24@navgurukul.org>
2026-02-07 10:56:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors({
-  origin:"https://smart-video-learning-assistant.onrender.com/"
+  origin:"https://smart-video-learning-assistant.onrender.com"
 }));
 app.use(express.json());
 
